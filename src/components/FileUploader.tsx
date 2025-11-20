ed19464117935df0dc4348d054b3f6159d8f1f4b
VictorAkubo <138879202+VictorAkubo@users.noreply.github.com>
2025-11-20 09:43:36 +0100

    Refactor FileUploader to remove unused imports

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,4 +1,4 @@
-import {useState, useCallback} from 'react'
+import {useCallback} from 'react'
 import {useDropzone} from 'react-dropzone'
 import { formatSize } from '../lib/utils'
 
@@ -15,7 +15,7 @@ const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
 
     const maxFileSize = 20 * 1024 * 1024; // 20MB in bytes
 
-    const {getRootProps, getInputProps, isDragActive, acceptedFiles} = useDropzone({
+    const {getRootProps, getInputProps, acceptedFiles} = useDropzone({
         onDrop,
         multiple: false,
         accept: { 'application/pdf': ['.pdf']},
@@ -45,7 +45,9 @@ const FileUploader = ({ onFileSelect }: FileUploaderProps) => {
                                     </p>
                                 </div>
                             </div>
+                            
                             <button className="p-2 cursor-pointer" onClick={(e) => {
+                            e.preventDefault()
                                 onFileSelect?.(null)
                             }}>
                                 <img src="/icons/cross.svg" alt="remove" className="w-4 h-4" />
